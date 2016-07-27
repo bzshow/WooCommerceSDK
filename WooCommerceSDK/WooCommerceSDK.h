f5740b049aaa55562a1bb878039478515ab63fa1
Sergio Utama <sergio.utama@gmail.com>
2016-07-27 22:19:41 +0800

    Initial Commit

---
+++ WooCommerceSDK/WooCommerceSDK.h
@@ -0,0 +1,19 @@
+//
+//  WooCommerceSDK.h
+//  WooCommerceSDK
+//
+//  Created by Sergio Utama on 27/07/2016.
+//  Copyright © 2016 Sergio Utama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WooCommerceSDK.
+FOUNDATION_EXPORT double WooCommerceSDKVersionNumber;
+
+//! Project version string for WooCommerceSDK.
+FOUNDATION_EXPORT const unsigned char WooCommerceSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WooCommerceSDK/PublicHeader.h>
+
+
